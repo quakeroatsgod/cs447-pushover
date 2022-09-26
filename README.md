ff61653922ef42e3e54d47727a418f24863c3f54
quakeroatsgod <agnellogriffen@gmail.com>
2022-09-26 12:16:10 -0700

    Solved issue 1. Changed license because I have to

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cs447-pushover
 A Java Pac-Man style game about pushing blocks made with the Slick2D game engine.
+
+## Attributions
+- Snowball textures taken from https://opengameart.org
+- Character sprites licensed under CC BY-SA 3.0 taken from https://route1rodent.itch.io/16x16-rpg-character-sprite-sheet 
--- src/pushover/Pushover.java
+++ src/pushover/Pushover.java
@@ -12,13 +12,17 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class Pushover extends StateBasedGame {
     public static final int STARTUPSTATE = 0;
-    public static final int READYUPSTATE  = 1;
+    public static final int PLAYINGSTATE  = 1;
     public int level=1;
     public final int ScreenWidth;
     public final int ScreenHeight;
+
+    //Resource strings
+    public static final String STARTUP_SCREEN_RES = "pushover/res/startup.png";
+    
     /*
     * Creates the Pushover game frame.
-    * */
+    */
     public Pushover(String title, int width, int height){
         super(title);
         ScreenHeight = height;
@@ -28,16 +32,17 @@ public class Pushover extends StateBasedGame {
     @Override
     public void initStatesList(GameContainer container) throws SlickException {
         addState(new StartUpState());
+        addState(new PlayingState());
 
         //Preload resources here
-
+        ResourceManager.loadImage(STARTUP_SCREEN_RES);
     }
 
     public static void main(String[] args){
         AppGameContainer app;
         try{
-            app = new AppGameContainer((new Pushover("Pushover", 800, 600)));
-            app.setDisplayMode(800,600,false);
+            app = new AppGameContainer((new Pushover("Pushover", 1000, 800)));
+            app.setDisplayMode(1000,800,false);
             Entity.antiAliasing=false;
             app.setVSync(true);
             app.start();
