cf382b8c021e95fbc2cd424ad90cd9c7123c86e0
quakeroatsgod <agnellogriffen@gmail.com>
2022-10-03 09:46:04 -0700

    finalized border wall placement

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 A Java Pac-Man style game about pushing blocks made with the Slick2D game engine.
 
 ## Attributions
-- Snowball textures taken from https://opengameart.org
+- Snowball, wall textures taken from https://opengameart.org
 - Character sprites licensed under CC BY-SA 3.0 taken from https://route1rodent.itch.io/16x16-rpg-character-sprite-sheet 
--- src/pushover/Pushover.java
+++ src/pushover/Pushover.java
@@ -10,16 +10,18 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+
 public class Pushover extends StateBasedGame {
     public static final int STARTUPSTATE = 0;
     public static final int PLAYINGSTATE  = 1;
     public int level=1;
     public final int ScreenWidth;
     public final int ScreenHeight;
-
+    public ArrayList<Grid> grid;
     //Resource strings
     public static final String STARTUP_SCREEN_RES = "pushover/res/startup.png";
-    
+    public static final String WALL_RES = "pushover/res/wall.png";
+    public static final String BLANK_RES = "pushover/res/blank-1.png";
     /*
     * Creates the Pushover game frame.
     */
@@ -28,6 +30,7 @@ public class Pushover extends StateBasedGame {
         ScreenHeight = height;
         ScreenWidth = width;
         Entity.setCoarseGrainedCollisionBoundary(Entity.AABB);
+        grid = new ArrayList<Grid>(50);
     }
     @Override
     public void initStatesList(GameContainer container) throws SlickException {
@@ -36,13 +39,17 @@ public class Pushover extends StateBasedGame {
 
         //Preload resources here
         ResourceManager.loadImage(STARTUP_SCREEN_RES);
+        ResourceManager.loadImage(WALL_RES);
+        ResourceManager.loadImage(BLANK_RES);
+
     }
 
     public static void main(String[] args){
         AppGameContainer app;
         try{
-            app = new AppGameContainer((new Pushover("Pushover", 1000, 800)));
-            app.setDisplayMode(1000,800,false);
+            //512 +20 buffer space
+            app = new AppGameContainer((new Pushover("Pushover", 660, 692)));
+            app.setDisplayMode(660,692,false);
             Entity.antiAliasing=false;
             app.setVSync(true);
             app.start();
